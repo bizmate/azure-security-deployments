7f67ba1f6c7ffa8b1e6a4e1213a6777ff9eb5cc7
bizmate <diego_gullo@bizmate.biz>
2024-03-21 02:09:06 +0400

    Fix filebeat config ELK host replacement and use the same template from the current version of filebeat used

--- siem-and-monitoring-alerts/provision-apache.sh
+++ siem-and-monitoring-alerts/provision-apache.sh
@@ -11,9 +11,9 @@ sudo dpkg -i filebeat-7.4.0-amd64.deb
 cd /etc/filebeat
 sudo mv filebeat.yml filebeat.yml_backup
 #edit filebeat.yml and change the value for output.elasticsearch and setup.kibana to reflect the IP of your Elk server
-sudo curl -o filebeat.yml https://raw.githubusercontent.com//bizmate/azure-security-deployments/filebeat-template.yml
+sudo curl -o filebeat.yml https://raw.githubusercontent.com/bizmate/azure-security-deployments/main/siem-and-monitoring-alerts/filebeat-template.yml
 echo "Adding ELK IP's to filebeat config"
-sudo sed -i 's/SIEM_ELASTICSEARCH_HOST/$SIEM_ELK_PRIVATE_IP/g' filebeat.yml
+sudo sed -i 's/SIEM_ELASTICSEARCH_HOST/'"$SIEM_ELK_PRIVATE_IP"'/g' filebeat.yml
 sudo filebeat modules enable system
 sudo filebeat modules enable apache
 sudo filebeat setup
